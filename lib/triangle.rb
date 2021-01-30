381fddd3b1b1936b96148cf3aeb15480adfcff11
remote-torrent-6747 <abbyc@sky.com>
2021-01-30 11:29:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
     @length3 = length3
   end
 
-  def kind(length1, length2, length3)
+  def kind(triangle)
     if length1 < 0 ||length2 < 0 || length3 < 0 #all the lengths are not positive OR less than 0
       begin
        raise TriangleError
@@ -17,7 +17,7 @@ class Triangle
       begin
        raise TriangleError
      rescue TriangleError
-     end 
+     end
     elsif length1 == length2 && length2 == length3
       return :equilateral
     elsif length1 == length2 || length1 == length3 || length2 == length3
